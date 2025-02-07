946cf69d90289c1297a1ec4ab55b1def351f803d
worthant <b_dvorkin@niuitmo.ru>
2025-02-08 01:56:10 +0300

    :hammer: feat(dfs.ts): Intuitive iterative dfs approach using stack

---
+++ dfs.ts
@@ -0,0 +1,34 @@
+// intuitive iterative approach using stack
+export function dfs<T>(graph: Map<T, T[]>, start: T): T[] {
+	const visited = new Set<T>(),
+		trace: T[] = [],
+		stack: T[] = [];
+
+	visited.add(start);
+	stack.push(start);
+	trace.push(start);
+
+	while (stack.length) {
+		const current = stack.pop()!;
+		for (const child of graph.get(current) || []) {
+			if (!visited.has(child)) {
+				visited.add(child);
+				stack.push(child);
+				trace.push(child);
+			}
+		}
+	}
+
+	return trace;
+}
+
+// example usage
+const tree = new Map<number, number[]>([
+	[1, [2, 3]],
+	[2, [4, 5]],
+	[3, [6]],
+	[4, []],
+	[5, []],
+	[6, []],
+]);
+console.log(dfs(tree, 1));
