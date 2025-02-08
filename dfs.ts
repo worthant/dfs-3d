7f288f8c0b53a29accc48963df943e54039c8f3b
worthant <b_dvorkin@niuitmo.ru>
2025-02-08 10:08:39 +0300

    :hammer: chore(dfs.ts): Example with diff. node types

--- dfs.ts
+++ dfs.ts
@@ -32,3 +32,10 @@ const tree = new Map<number, number[]>([
 	[6, []],
 ]);
 console.log(dfs(tree, 1));
+
+// want graph with different types?
+type Node = number | string | Set<number>
+const tree2 = new Map<Node, Node[]>([
+    [1, ['2', 34, new Set([5, 6])]]
+])
+console.log(dfs(tree2, 1))
